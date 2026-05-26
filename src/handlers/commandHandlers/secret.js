fc89cb5e6ac2460ef740f395cb2087fda670034a
alex7819000-sys <alex7819000@gmail.com>
2026-05-26 11:02:33 +0200

    Delete src directory

--- src/handlers/commandHandlers/secret.js
+++
@@ -1,5 +0,0 @@
-// src/handlers/commandHandlers/secret.js
-'use strict';
-const { openModal } = require('../../systems/secret');
-async function handle(interaction) { return openModal(interaction); }
-module.exports = { handle };
