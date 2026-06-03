6e28a54921e7b97ffa66c2dd91a7134502164cd0
alex7819000-sys <alex7819000@gmail.com>
2026-06-03 12:21:35 +0200

    Delete src directory

--- src/handlers/commandHandlers/debat.js
+++
@@ -1,8 +0,0 @@
-// src/handlers/commandHandlers/debat.js
-'use strict';
-const { openDebatModal } = require('../../systems/debat');
-async function handle(interaction) {
-  const sub = interaction.options.getSubcommand();
-  if (sub === 'creer') return openDebatModal(interaction);
-}
-module.exports = { handle };
