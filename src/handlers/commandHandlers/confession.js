6aa94b5572b42a97101e4fc0cb3dbc502e0f8214
alex7819000-sys <alex7819000@gmail.com>
2026-06-04 14:12:05 +0200

    Delete src directory

--- src/handlers/commandHandlers/confession.js
+++
@@ -1,5 +0,0 @@
-// src/handlers/commandHandlers/confession.js
-'use strict';
-const { openConfessionModal } = require('../../systems/confession');
-async function handle(interaction) { return openConfessionModal(interaction); }
-module.exports = { handle };
