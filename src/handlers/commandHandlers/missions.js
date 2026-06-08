bb9d43b86e5067272edd0d8b7705b9fb87e1843d
alex7819000-sys <alex7819000@gmail.com>
2026-06-08 14:37:29 +0200

    Delete src directory

--- src/handlers/commandHandlers/missions.js
+++
@@ -1,9 +0,0 @@
-// src/handlers/commandHandlers/missions.js
-'use strict';
-const { getDailyMissionsEmbed } = require('../../systems/dailymissions');
-
-module.exports = async function handleMissions(interaction) {
-  await interaction.deferReply({ ephemeral: false });
-  const embed = await getDailyMissionsEmbed(interaction.user.id, interaction.guild.id);
-  return interaction.editReply({ embeds: [embed] });
-};
