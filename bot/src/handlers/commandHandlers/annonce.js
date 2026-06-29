a191687cf8631bf21f5fcd74684e4f38e871d801
alex7819000-sys <alex7819000@gmail.com>
2026-06-29 19:15:29 +0200

    Delete bot/src directory

--- bot/src/handlers/commandHandlers/annonce.js
+++
@@ -1,6 +0,0 @@
-// src/handlers/commandHandlers/annonce.js
-'use strict';
-const { postAnnonce } = require('../../systems/animation');
-module.exports = async function handleAnnonce(interaction, client) {
-  return postAnnonce(interaction, client);
-};
