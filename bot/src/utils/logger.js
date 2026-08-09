26718102a37d86242c015ce1b971dc213e2ca1ed
alex7819000-sys <alex7819000@gmail.com>
2026-08-09 14:31:35 +0200

    Delete bot directory

--- bot/src/utils/logger.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-const logger = {
-  info:  (tag, msg, ...args) => console.log(`[INFO][${tag}] ${msg}`, ...args),
-  error: (tag, msg, ...args) => console.error(`[ERROR][${tag}] ${msg}`, ...args),
-  warn:  (tag, msg, ...args) => console.warn(`[WARN][${tag}] ${msg}`, ...args),
-  debug: (tag, msg, ...args) => console.debug(`[DEBUG][${tag}] ${msg}`, ...args),
-};
-module.exports = logger;
