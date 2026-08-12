624fb133751bcfa95b306a4dccdb4ec6224f4716
alex7819000-sys <alex7819000@gmail.com>
2026-08-12 07:05:52 +0200

    Delete bot directory

--- bot/src/db/models/Nomination.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-const mongoose = require('mongoose');
-const schema = new mongoose.Schema({
-  guildId: String, nominatorId: String, targetId: String,
-  type: String, week: Number, year: Number,
-}, { timestamps: true });
-module.exports = mongoose.models.Nomination || mongoose.model('Nomination', schema);
